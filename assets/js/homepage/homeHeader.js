30f040bf3bbb86a77a48618e36012881eba5e086
William Bradshaw <will.bradshaw50@gmail.com>
2022-07-09 14:03:55 +1000

    reduced motion checks and fallbacks for animations

--- assets/js/homepage/homeHeader.js
+++ assets/js/homepage/homeHeader.js
@@ -1,51 +1,57 @@
-var amount = (-(window.innerHeight/2)+(window.innerHeight/3.7) + "px")
+var amount = -(window.innerHeight / 2) + window.innerHeight / 3.7 + "px";
 
 const guys = document.querySelectorAll(".homeHeader__feature__guy");
-const observerGuys = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        entry.target.classList.toggle("reveal", entry.isIntersecting)
-    })
-},
-{
-    rootMargin: amount
-})
+const observerGuys = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      entry.target.classList.toggle("reveal", entry.isIntersecting);
+    });
+  },
+  {
+    rootMargin: amount,
+  }
+);
 
-guys.forEach(guy => {
-    observerGuys.observe(guy)
-})
-
-/*var feature = document.getElementById("feature");
-var line = document.getElementById("line");
-var length = line.getTotalLength();
-var draw, length;
-line.style.strokeDasharray = length;
-line.style.strokeDashoffset = length;
-var clientHeight = document.documentElement.clientHeight;
-window.addEventListener("scroll", function(){
-    requestAnimationFrame(drawLine);
+guys.forEach((guy) => {
+  observerGuys.observe(guy);
 });
 
-function drawLine() {
-    var scrollRatio = (document.documentElement.scrollTop) / (document.documentElement.scrollHeight - clientHeight);
-    if (scrollRatio > 1) {
-        scrollRatio = 1;
-    }
-    draw = length * scrollRatio;
-    console.log(draw)
-    console.log(length)
-    console.log(length - draw)
-    line.style.strokeDashoffset = length - draw;
-}*/
-
-let observerText = new IntersectionObserver(entries => {
-    if (entries[0].boundingClientRect.y < 0) {
-        document.getElementById("scrollingText").classList.remove("showY");
-        setTimeout(stopAnim => {
-            document.getElementById("scrollingText__text").style.animationPlayState = 'paused';
-        }, 1000)
-    } else {
-        document.getElementById("scrollingText__text").style.animationPlayState = 'running';
-        document.getElementById("scrollingText").classList.add("showY");
-    }
+const isReduced = window.matchMedia(`(prefers-reduced-motion: reduce)`) === true || window.matchMedia(`(prefers-reduced-motion: reduce)`).matches === true;
+if ((!CSS.supports("animation-timeline: pathTimeline")) && (!isReduced)) {
+  var feature = document.getElementById("feature");
+  var line = document.getElementById("line");
+  var length = line.getTotalLength();
+  var draw, length;
+  line.style.strokeDasharray = length;
+  line.style.strokeDashoffset = length;
+  var clientHeight = document.documentElement.clientHeight;
+  window.addEventListener("scroll", function () {
+    requestAnimationFrame(drawLine);
   });
+}
+
+function drawLine() {
+  var scrollRatio =
+    document.documentElement.scrollTop /
+    (document.documentElement.scrollHeight - clientHeight);
+  if (scrollRatio > 1) {
+    scrollRatio = 1;
+  }
+  draw = length * scrollRatio;
+  line.style.strokeDashoffset = length - draw;
+}
+
+let observerText = new IntersectionObserver((entries) => {
+  if (entries[0].boundingClientRect.y < 0) {
+    document.getElementById("scrollingText").classList.remove("showY");
+    setTimeout((stopAnim) => {
+      document.getElementById("scrollingText__text").style.animationPlayState =
+        "paused";
+    }, 1000);
+  } else {
+    document.getElementById("scrollingText__text").style.animationPlayState =
+      "running";
+    document.getElementById("scrollingText").classList.add("showY");
+  }
+});
 observerText.observe(document.querySelector("#scrollTrigger"));
